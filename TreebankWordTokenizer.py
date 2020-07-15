22200355ae134f4c1cb0e97d751d209a02dc5520
embedded1996 <madetej626@mailfile.org>
2020-07-15 21:33:49 +0900

    TreebankWordTokenizer

---
+++ TreebankWordTokenizer.py
@@ -0,0 +1,4 @@
+from nltk.tokenize import TreebankWordTokenizer
+tokenizer=TreebankWordTokenizer()
+text="Starting a home-based restaurant may be an ideal. it doesn't have a food chain or restaurant of their own."
+print(tokenizer.tokenize(text))
